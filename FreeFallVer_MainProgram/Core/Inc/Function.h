51a4737f21a560584895735e9d338b34a82d645e
unknown <thanhnguyenvan0596@gmail.com>
2022-12-26 17:49:21 +0900

    Update

---
+++ FreeFallVer_MainProgram/Core/Inc/Function.h
@@ -0,0 +1,44 @@
+#include "stdint.h"
+#include "stdbool.h"
+#include "main.h"
+
+// Constant parameter
+#define DriverID 1
+#define TachometerID 2
+#define P402_SpeedCommand 401 // Register address of the parameter P402
+#define StE03 12 // Register for motor speed in rpm
+#define StE07 16 // Encoder/Feedback pulse register
+#define StE08 17 // Error pulse
+
+#define ASDA_EncoderRegister 20 // 0x014h = 20, ASDA Driver's Status Mornitoring Register 2 - Encoder Pulses
+#define ASDA_MotorSpeed 18 // 0x012h = 18, ASDA Driver's Status Mornitoring Register 1 - Motor Speed
+#define CounterA 1 // Counter A high word register
+
+#define Timer2Period 1 // ms, timer2 period
+#define Timer3Period 15 // us, timer3 period
+#define DataSampleTime 50 // ms
+#define HigenEncoderResolution 8192
+#define AsdaEncoderResolution 1024
+
+#define RampingGoingSpdTime 3 // seconds
+
+
+#define DroppingMode 1
+#define PullingMode 2
+#define PullAndDropMode 3
+
+// Address to save the parameters Sector5
+//#define MemoryAddress 0x0800C100 // Sector5
+
+#define MemoryAddress 0x08040000 //
+
+void Stop(); // Stop motor function
+void AlarmReset(); // reset alarm function
+void Estop(); // Estop function
+void JogMoveUp();
+void JogMoveDown();
+void DisableSTOP();
+void SetPositionMode();
+void SetSpeedMode();
+bool DriverInit();
+uint16_t ReadLogicF7000Out(void);
---
+++ FreeFallVer_MainProgram/Debug/Core/Inc/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/Inc/FLASH_SECTOR_F4.c \
+../Core/Inc/Function.c 
+
+OBJS += \
+./Core/Inc/FLASH_SECTOR_F4.o \
+./Core/Inc/Function.o 
+
+C_DEPS += \
+./Core/Inc/FLASH_SECTOR_F4.d \
+./Core/Inc/Function.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Inc/%.o Core/Inc/%.su: ../Core/Inc/%.c Core/Inc/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F407xx -c -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Core-2f-Inc
+
+clean-Core-2f-Inc:
+	-$(RM) ./Core/Inc/FLASH_SECTOR_F4.d ./Core/Inc/FLASH_SECTOR_F4.o ./Core/Inc/FLASH_SECTOR_F4.su ./Core/Inc/Function.d ./Core/Inc/Function.o ./Core/Inc/Function.su
+
+.PHONY: clean-Core-2f-Inc
+
